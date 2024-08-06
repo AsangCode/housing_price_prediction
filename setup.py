a663bc9d394be87505fc4cf37b33cba3454d471b
AsangCode <150847744+AsangCode@users.noreply.github.com>
2024-08-06 15:26:45 +0530

    setup.py is added

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ long_description = (Path(__file__).parent / 'README.md').read_text()
 setup(
     name="Housing_Price_Prediction",
     version="2.0.1",
-    author="Asang Kumar Singh",
-    author_email="asangkumar6666@gmail.com",
+    author="Asang Singh",
+    author_email="2022ac05454@wilp.bits-pilani.ac.in",
     description="A package for predicting housing prices",
     long_description=long_description,
     long_description_content_type='text/markdown',  # Using Markdown content type for .md files
