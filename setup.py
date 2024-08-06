f0395844f1c9ec198d441da15b32f8a9d0490604
AsangCode <150847744+AsangCode@users.noreply.github.com>
2024-08-06 13:32:12 +0530

    setup.py is edited

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import find_packages, setup
-from typing import List
+from pathlib import Path
 
+# Read the content of your README file
+long_description = (Path(__file__).parent / 'README.txt').read_text()
 
 setup(
     name="HousingPricePrediction",
     version="0.0.1",
     author="Asang Kumar Singh",
     author_email="asangkumar6666@gmail.com",
-    install_requires=["pytest", "pandas", "numpy", "ensure","scikit-learn", "tox"],
-    packages=find_packages()
+    description="A package for predicting housing prices",
+    long_description=long_description,
+    long_description_content_type='text/plain',  # Using plain text since README.txt is a .txt file
+    install_requires=["pytest", "pandas", "numpy", "ensure", "scikit-learn", "tox"],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
